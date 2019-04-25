bc2cae298340e5fa2f65f9e616e39b64550aa4f9
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-04-25 10:37:56 +0200

    Add VueBootstrap.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import store from './store'
 import './registerServiceWorker'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
   store,
