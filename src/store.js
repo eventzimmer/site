ab26eb9402bf040647b79dd452b6502fc56bc0b7
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-04-25 13:56:46 +0200

    Add needed store methods.

--- src/store.js
+++ src/store.js
@@ -3,14 +3,27 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const ENDPOINT = (process.env.VUE_APP_ENDPOINT !== undefined) ? process.env.VUE_APP_ENDPOINT : 'http://localhost:8080/v1'
+
 export default new Vuex.Store({
   state: {
-
+    location: {
+      name: 'Tübingen',
+      latitude: 48.52,
+      longitude: 9.05556
+    },
+    events: []
   },
   mutations: {
-
+    addEvents (state, events) {
+      state.events = events
+    }
   },
   actions: {
-
+    fetchEvents (context) {
+      fetch(`${ENDPOINT}/events`)
+        .then((events) => context.commit('addEvents', events))
+        .catch((err) => console.error(err))
+    }
   }
 })
