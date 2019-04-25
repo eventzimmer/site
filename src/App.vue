c3c9ab1091b534436efedd8f13a75b70f1782e70
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-04-25 15:16:49 +0200

    Decide for a structure and color scheme for now.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,48 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app d-flex flex-column h-100">
+    <main role="main" class="flex-shrink-0 w-auto">
+      <div class="container w-auto">
+        <img alt="eventzimmer logo" src="./assets/icon-192x192.png" class="logo mt-3">
+        <h3 class="mt-5">eventzimmer</h3>
+        <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
+      </div>
+    </main>
+    <footer class="footer mt-auto py-3 fixed-bottom">
+      <hr />
+      <div class="container">
+        <span class="text-muted">Impressum | Datenschutzerklärung</span>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+html {
+  background-color: rgba(245,245,245, 0.6);
+  /*background-image: linear-gradient(to bottom, #358fa1, #74a6bd, #a8bdd3, #d3d7e4, #f5f5f5) !important;*/
+}
+
+main {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.logo {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+body {
+  background-color: transparent !important;
+}
+
+#app {
+
 }
 </style>
---
+++ src/components/Events.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Events.vue',
+  data () {
+    return {
+      loading: true
+    }
+  },
+  mounted () {
+    let vm = this
+    vm.$store.dispatch('fetchEvents').then(() => {
+      vm.loading = false
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
