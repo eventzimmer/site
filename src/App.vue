9359b2d9be6381696f521735a2a9573be87ffbff
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-04-26 20:02:16 +0200

    Move bootstrap structure to index.html.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <template>
-  <div id="app d-flex flex-column h-100">
-    <main role="main" class="flex-shrink-0 w-auto">
-      <div class="container w-auto">
-        <img alt="eventzimmer logo" src="./assets/icon-192x192.png" class="logo mt-3">
-        <h3 class="mt-5">eventzimmer</h3>
-        <p class="lead">Veranstaltungen im {{ month() }}</p>
-        <events></events>
-      </div>
-    </main>
-    <footer class="footer mt-auto py-3">
-      <hr />
-      <div class="container">
-        <span class="text-muted">Impressum | Datenschutzerklärung</span>
-      </div>
-    </footer>
+  <div id="app">
+    <h3 class="mt-5">eventzimmer</h3>
+    <p class="lead">Veranstaltungen im {{ month() }}</p>
+    <events></events>
   </div>
 </template>
 
@@ -34,24 +23,3 @@ export default {
   }
 }
 </script>
-
-<style>
-html {
-  background-color: rgba(245,245,245, 0.6);
-  /*background-image: linear-gradient(to bottom, #358fa1, #74a6bd, #a8bdd3, #d3d7e4, #f5f5f5) !important;*/
-}
-
-.logo {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-body {
-  background-color: transparent !important;
-}
-
-#app {
-
-}
-</style>
