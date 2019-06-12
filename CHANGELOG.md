f0df4ec8619b6ffa47ad757f9eec4475fcb98e49
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-06-12 12:21:39 +0200

    Add badge to show remaining time until event.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,9 +5,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+## [0.0.3] - 2019-06-12
 ### Added
 - Added font-awesome and instagram icon + link
 - Added SEO meta tags
+- Add badge with remaining time until event
 
 ### Changed
 - Decode html containing description
--- src/components/Events.vue
+++ src/components/Events.vue
@@ -4,6 +4,7 @@
     <div class="card mb-1" v-for="event in $store.getters.currentMonthEvents" v-bind:key="event.url">
       <div class="card-body">
         <a :href="event.url" class="card-link" target="_blank" rel="noopener"><h6 class="card-subtitle mb-2 text-muted">{{ event.name }}
+          <span class="badge badge-pill badge-secondary ml-1">{{ distanceInWordsToNow(event.starts_at) }}</span>
           <span class="badge badge-pill badge-secondary ml-1">{{ formatEventDate(event.starts_at) }}</span>
           <span class="badge badge-pill badge-secondary ml-1">{{ event.location.name }}</span>
         </h6></a>
@@ -14,6 +15,7 @@
     <div class="card mb-1" v-for="event in $store.getters.nextMonthEvents" v-bind:key="event.url">
       <div class="card-body">
         <a :href="event.url" class="card-link" target="_blank" rel="noopener"><h6 class="card-subtitle mb-2 text-muted">{{ event.name }}
+          <span class="badge badge-pill badge-secondary ml-1">{{ distanceInWordsToNow(event.starts_at) }}</span>
           <span class="badge badge-pill badge-secondary ml-1">{{ formatEventDate(event.starts_at) }}</span>
           <span class="badge badge-pill badge-secondary ml-1">{{ event.location.name }}</span>
         </h6></a>
@@ -28,7 +30,7 @@
 
 <script>
 import truncate from 'vue-truncate-collapsed'
-import { format, addMonths } from 'date-fns'
+import { format, addMonths, distanceInWordsToNow } from 'date-fns'
 import de from 'date-fns/locale/de'
 
 export default {
@@ -48,6 +50,9 @@ export default {
     })
   },
   methods: {
+    distanceInWordsToNow (date) {
+      return distanceInWordsToNow(date, { locale: de, addSuffix: true })
+    },
     htmlDecode (value) {
       return $('<div/>').html(value).text() // eslint-disable-line no-undef
     },
