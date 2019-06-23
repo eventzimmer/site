6eaa0bb13640b6c6e65b275b6252299f1d22fb6e
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-06-23 15:19:18 +0200

    Move labels out of title.

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="card mb-1">
     <div class="card-body">
-      <a :href="event.url" class="card-link" target="_blank" rel="noopener"><h6 class="card-subtitle mb-2 text-muted">{{ event.name }}
-        <span class="badge badge-pill badge-secondary ml-1">{{ distanceInWordsToNow(event.starts_at) }}</span>
-        <span class="badge badge-pill badge-secondary ml-1">{{ formatEventDate(event.starts_at) }}</span>
-        <span class="badge badge-pill badge-secondary ml-1 text-truncate" style="max-width: 100%">{{ event.location.name }}</span>
-      </h6></a>
+      <a :href="event.url" class="card-link" target="_blank" rel="noopener">
+        <h6 class="card-subtitle mb-1 text-muted">
+          {{ event.name }}
+        </h6>
+        <span class="badge badge-pill badge-secondary overflow-auto">{{ distanceInWordsToNow(event.starts_at) }}</span>
+        <span class="badge badge-pill badge-secondary ml-1 overflow-auto">{{ formatEventDate(event.starts_at) }}</span>
+        <span class="badge badge-pill badge-secondary ml-1 d-inline-block text-truncate" style="max-width: 100%">{{ event.location.name }}</span>
+        <br/>
+      </a>
       <span v-html="htmlDecode(event.description.substring(0, (collapsed) ? 200 : event.description.length))"></span>
       <button type="button" class="btn btn-outline-secondary btn-sm ml-1" @click="collapsed = !collapsed">{{ (collapsed) ? 'Mehr' : 'Weniger' }}</button>
     </div>
