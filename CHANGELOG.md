de3f104f11859a2e3ef6f8af5a9da7b8dfd0e4d8
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-06-24 17:23:53 +0200

    Add back to top button. Closes #15.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Added
 - Persist selected city
+- Added a back to top button
 
 ### Changed
 - Use Matomo with JavaScript and provide an explicit opt-out link in the privacy page.
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h3 class="mt-5">eventzimmer</h3>
+    <button type="button" @click="backToTop" id="backToTop" class="btn btn-secondary btn-back-to-top fixed-bottom mb-2" style="display: none"><i class="fas fa-angle-up"></i></button>
     <cities></cities>
     <events></events>
   </div>
@@ -15,6 +16,26 @@ export default {
   components: {
     Events,
     Cities
+  },
+  mounted () {
+    window.onscroll = () => {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 100) {
+        document.getElementById('backToTop').style.display = 'block'
+      } else {
+        document.getElementById('backToTop').style.display = 'none'
+      }
+    }
+  },
+  methods: {
+    backToTop () {
+      window.scrollTo(0, 0)
+    }
   }
 }
 </script>
+
+<style scoped>
+.btn-back-to-top {
+  left: 85%;
+}
+</style>
