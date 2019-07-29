b62f8256bc0f144a2ddb425427871729a5ff2c3c
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-07-29 21:13:29 +0200

    Add tags to events.

--- src/store.js
+++ src/store.js
@@ -9,9 +9,24 @@ Vue.use(Vuex)
 const PAST_HOUR_LIMIT = 3 // How many hours in the past would we like to include on the current day. In the future this may additionally depend on the length of the event.
 const ENDPOINT = (process.env.VUE_APP_ENDPOINT !== undefined) ? process.env.VUE_APP_ENDPOINT : 'http://localhost:8080/v1'
 const FIXTURE_EVENTS = require('@/assets/events.json')
+const LOCATIONS_ANNOTATED = require('@/assets/locations_annotated.json')
+const TAGS_PER_LOCATION = LOCATIONS_ANNOTATED
+  .map((location) => {
+    return {
+      name: location.name,
+      tags: location.tags.split(',').filter((part) => part.length)
+    }
+  })
+  .reduce((accumulator, location) => {
+    accumulator[location.name] = location.tags
+    return accumulator
+  }, {})
 
 export default new Vuex.Store({
   state: {
+    selection: {
+      categories: []
+    },
     location: {
       name: 'Tübingen',
       latitude: 48.52,
@@ -66,6 +81,12 @@ export default new Vuex.Store({
       let currentDay = getDate(today)
       let currentMonth = getMonth(today)
       return getters.events.filter((e) => (getMonth(e.starts_at) === currentMonth) && (getDate(e.starts_at) >= currentDay) && ((getDate(e.starts_at) === currentDay) ? (today.getHours() - e.starts_at.getHours() <= PAST_HOUR_LIMIT) : true))
+    },
+    annotatedEvents (state, getters) {
+      return getters.events.map((event) => {
+        event.tags = TAGS_PER_LOCATION[event.location.name]
+        return event
+      })
     },
     events (state) {
       let events = state.events.map((e) => {
