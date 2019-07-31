4bd15e5ec04a8268543cef04aa6a5b9562775e25
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-07-31 09:56:12 +0200

    Only show category if event has a single category.

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -8,7 +8,7 @@
         <span class="badge badge-pill badge-secondary ml-1 overflow-auto">{{ distanceInWordsToNow(event.starts_at) }}</span>
         <span class="badge badge-pill badge-secondary ml-1 overflow-auto">{{ formatEventDate(event.starts_at) }}</span>
         <span class="badge badge-pill badge-secondary ml-1 d-inline-block text-truncate" style="max-width: 100%">{{ event.location.name }}</span>
-        <span class="badge badge-pill badge-secondary ml-1 overflow-auto" v-for="category in event.categories">{{ category }}</span>
+        <span class="badge badge-pill badge-secondary ml-1 overflow-auto" v-if="event.categories.length === 1">{{ event.categories[0] }}</span>
         <br/>
       </a>
       <span v-html="htmlDecode(event.description.substring(0, (collapsed) ? 200 : event.description.length))"></span>
