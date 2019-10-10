39870d5c74d668ff5daeb9a95df8de0e19e68f65
Pooja Prasad <poojakprasad94@gmail.com>
2019-10-10 08:49:41 +0530

    fix indentation

--- src/store/index.js
+++ src/store/index.js
@@ -21,85 +21,86 @@ const CATEGORIES_PER_LOCATION = LOCATIONS_ANNOTATED
     accumulator[location.name] = location.tags
     return accumulator
   }, {})
-  export default new Vuex.Store({
-    state: {
-      location: {
-        name: 'Tübingen',
-        latitude: 48.52,
-        longitude: 9.05556
-      },
-      events: []
+
+export default new Vuex.Store({
+  state: {
+    location: {
+      name: 'Tübingen',
+      latitude: 48.52,
+      longitude: 9.05556
     },
-    mutations: {
-      changeEvents (state, events) {
-        state.events = events
-      },
-      changeLocation (state, location) {
-        state.location = location
-      }
+    events: []
+  },
+  mutations: {
+    changeEvents (state, events) {
+      state.events = events
     },
-    actions: {
-      fetchEvents (context) {
-        // eslint-disable-next-line no-undef
-        return fetch(`${ENDPOINT}/rpc/events_by_radius?${new URLSearchParams({
-          latitude: context.state.location.latitude,
-          longitude: context.state.location.longitude,
-          select: 'name,url,description,created_at,starts_at,location(*),source(*)',
-          order: 'starts_at'
-        })}`, {
-          'Content-Type': 'application/json'
-        }).then((response) => response.json())
-          .then((events) => context.commit('changeEvents', events))
-          .catch((err) => {
-            throw err
-          })
-      }
-    },
-    getters: {
-      nextMonthEvents (state, getters) {
-        const nextMonth = getMonth(addMonths(new Date(), 1))
-        return getters.eventsBySelectedCategories.filter((e) => getMonth(e.starts_at) === nextMonth)
-      },
-      currentMonthEvents (state, getters) {
-        const today = new Date()
-        const currentDay = getDate(today)
-        const currentMonth = getMonth(today)
-        return getters.eventsBySelectedCategories.filter((e) => (getMonth(e.starts_at) === currentMonth) && (getDate(e.starts_at) >= currentDay) && ((getDate(e.starts_at) === currentDay) ? (today.getHours() - e.starts_at.getHours() <= PAST_HOUR_LIMIT) : true))
-      },
-      eventsBySelectedCategories (state, getters) {
-        if (state.selection.categories.length) {
-          return getters.eventsWithCategories.filter((e) => e.categories.some((c) => state.selection.categories.includes(c)))
-        } else {
-          return getters.eventsWithCategories
-        }
-      },
-      eventsByRange (state, getters) {
-        return (state.selection.start !== null) ? getters.events.filter((e) => (e.starts_at >= getters.range.start) && (e.starts_at <= getters.range.end)) : getters.events
-      },
-      eventsWithCategories (state, getters) {
-        return getters.eventsByRange.map((event) => {
-          event.categories = CATEGORIES_PER_LOCATION[event.location.name]
-          return event
+    changeLocation (state, location) {
+      state.location = location
+    }
+  },
+  actions: {
+    fetchEvents (context) {
+      // eslint-disable-next-line no-undef
+      return fetch(`${ENDPOINT}/rpc/events_by_radius?${new URLSearchParams({
+        latitude: context.state.location.latitude,
+        longitude: context.state.location.longitude,
+        select: 'name,url,description,created_at,starts_at,location(*),source(*)',
+        order: 'starts_at'
+      })}`, {
+        'Content-Type': 'application/json'
+      }).then((response) => response.json())
+        .then((events) => context.commit('changeEvents', events))
+        .catch((err) => {
+          throw err
         })
-      },
-      events (state) {
-        return state.events.map((e) => {
-          e.starts_at = new Date(e.starts_at)
-          return e
-        })
-      },
-      categories () {
-        return new Set(Object.values(CATEGORIES_PER_LOCATION).reduce((acc, val) => acc.concat(val), [])) // NOTE: This can be replaced with Array.flat
+    }
+  },
+  getters: {
+    nextMonthEvents (state, getters) {
+      const nextMonth = getMonth(addMonths(new Date(), 1))
+      return getters.eventsBySelectedCategories.filter((e) => getMonth(e.starts_at) === nextMonth)
+    },
+    currentMonthEvents (state, getters) {
+      const today = new Date()
+      const currentDay = getDate(today)
+      const currentMonth = getMonth(today)
+      return getters.eventsBySelectedCategories.filter((e) => (getMonth(e.starts_at) === currentMonth) && (getDate(e.starts_at) >= currentDay) && ((getDate(e.starts_at) === currentDay) ? (today.getHours() - e.starts_at.getHours() <= PAST_HOUR_LIMIT) : true))
+    },
+    eventsBySelectedCategories (state, getters) {
+      if (state.selection.categories.length) {
+        return getters.eventsWithCategories.filter((e) => e.categories.some((c) => state.selection.categories.includes(c)))
+      } else {
+        return getters.eventsWithCategories
       }
     },
-    modules: {
-      selection: selection
+    eventsByRange (state, getters) {
+      return (state.selection.start !== null) ? getters.events.filter((e) => (e.starts_at >= getters.range.start) && (e.starts_at <= getters.range.end)) : getters.events
     },
-    plugins: [
-      createPersistedState({
-        key: 'eventzimmer',
-        paths: ['location', 'selection'],
-        filter: (mutation) => !['changeEvents'].includes(mutation.type)
+    eventsWithCategories (state, getters) {
+      return getters.eventsByRange.map((event) => {
+        event.categories = CATEGORIES_PER_LOCATION[event.location.name]
+        return event
       })
-    ]
-  })
+    },
+    events (state) {
+      return state.events.map((e) => {
+        e.starts_at = new Date(e.starts_at)
+        return e
+      })
+    },
+    categories () {
+      return new Set(Object.values(CATEGORIES_PER_LOCATION).reduce((acc, val) => acc.concat(val), [])) // NOTE: This can be replaced with Array.flat
+    }
+  },
+  modules: {
+    selection: selection
+  },
+  plugins: [
+    createPersistedState({
+      key: 'eventzimmer',
+      paths: ['location', 'selection'],
+      filter: (mutation) => !['changeEvents'].includes(mutation.type)
+    })
+  ]
+})
