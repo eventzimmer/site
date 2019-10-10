4b02cf0910759318e16ec0b054d258a8fc13f537
Pooja Prasad <poojakprasad94@gmail.com>
2019-10-10 08:49:41 +0530

    fix indentation

--- src/store/selection.js
+++ src/store/selection.js
@@ -1,34 +1,34 @@
 const selection = {
-    state: {
-      categories: [],
-      start: null,
-      end: null,
-      language: 'de'
+  state: {
+  categories: [],
+  start: null,
+  end: null,
+  language: 'de'
+  },
+  getters: {
+    range (state) {
+      return (state.start !== null) ? {
+        start: new Date(state.start),
+        end: new Date(state.end)
+      } : null
+    }
+  },
+  mutations: {
+    changeCategories (state, categories) {
+      state.categories = categories
     },
-    getters: {
-      range (state) {
-        return (state.start !== null) ? {
-          start: new Date(state.start),
-          end: new Date(state.end)
-        } : null
+    changeRange (state, range) {
+      if (range !== null) {
+        state.start = range.start.toISOString()
+        state.end = range.end.toISOString()
+      } else {
+        state.start = state.end = null
       }
     },
-    mutations: {
-      changeCategories (state, categories) {
-        state.categories = categories
-      },
-      changeRange (state, range) {
-        if (range !== null) {
-          state.start = range.start.toISOString()
-          state.end = range.end.toISOString()
-        } else {
-          state.start = state.end = null
-        }
-      },
-      changeLanguage (state, language) {
-        state.language = language
-      }
+    changeLanguage (state, language) {
+      state.language = language
     }
   }
+}
 
-  export default selection;
+export default selection;
