d2419a9eea8e3d27fd17b9e08dbd876d670dbadc
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-10-16 21:42:09 +0200

    Add Netlify link to footer.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,6 +5,7 @@
       <span class="text-dark">
       <a href="impressum.html" class="text-dark">{{ $t("msg.imprint") }}</a> |
       <a href="datenschutz.html" class="text-dark">{{ $t("msg.privacy") }}</a> |
+      <a href="https://www.netlify.com" class="text-dark mr-1">This site is powered by Netlify</a> <img src="../assets/full-logo-light-simple.svg" style="height: 1em"/> |
       <a href="https://www.instagram.com/eventzimmer_tuebingen/" aria-label="eventzimmer Instagram profile"><i class="fab fa-instagram text-dark"></i></a> |
       <a href="https://twitter.com/eventzimmer" aria-label="eventzimmer Twitter profile"><i class="fab fa-twitter text-dark"></i></a>
       </span>
