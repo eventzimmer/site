7d45b9bf974ffe0d61a921a99a271f8c0668e5ec
Sbaah <sbaah12@gmail.com>
2019-10-19 16:27:18 -0400

    Update Event.vue

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -7,11 +7,11 @@
         </h6>
         <span class="badge badge-pill badge-secondary ml-1 overflow-auto">{{ distanceInWordsToNow(event.starts_at) }}</span>
         <span class="badge badge-pill badge-secondary ml-1 overflow-auto">{{ formatEventDate(event.starts_at) }}</span>
-        <span class="badge badge-pill badge-secondary ml-1 d-inline-block text-truncate" style="max-width: 100%">{{ event.location.name }}</span>
+        <span class="badge badge-pill badge-secondary ml-1 d-inline-block text-truncate" style="max-width: 50%">{{ event.location.name }}</span>
         <span class="badge badge-pill badge-secondary ml-1 overflow-auto" v-if="event.categories.length === 1">{{ event.categories[0] }}</span>
         <br/>
       </a>
-      <span v-html="htmlDecode(event.description.substring(0, (collapsed) ? 200 : event.description.length))"></span>
+      <span v-html="htmlDecode(event.description.substring(0, (collapsed) ? 100 : event.description.length))"></span>
       <button type="button" v-if="event.description.length > max" class="btn btn-outline-secondary btn-sm ml-1" @click="collapsed = !collapsed">{{ $tc('msg.more_less', collapsed) }}</button>
     </div>
   </div>
